24408d3f7546aa970b7e8a72d3c0d9c3405fbfc0
Juan Martin Barril <jbarril@outlook.com>
2023-04-20 21:43:45 -0300

    Update hello.c

--- 00-CHelloworld/hello.c
+++ 00-CHelloworld/hello.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    FILE *fp;
-    fp = fopen("output.txt", "w");
-    fprintf(fp, "Hello, World!");
-    fclose(fp);
-    return 0;
+    printf ("Hello, World!");
 }
